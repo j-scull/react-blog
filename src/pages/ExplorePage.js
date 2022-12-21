89c6f510f5057111df55533477054083ae5bf4ce
j-scull <joe-scullion@hotmail.com>
2022-12-21 21:02:17 +0000

    Chapter 4: add routes.

---
+++ src/pages/ExplorePage.js
@@ -0,0 +1,12 @@
+import Body from '../components/Body';
+import Posts from '../components/Posts';
+
+export default function ExploePage() {
+    return (
+        <Body sidebar>
+            <h1>Explore</h1>
+            <p>ToDo</p>
+        </Body>
+    );
+
+}
