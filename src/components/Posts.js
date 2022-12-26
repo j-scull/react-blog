d2ef561bdb40016b8b88702162f2065a4f51d044
j-scull <joe-scullion@hotmail.com>
2022-12-26 14:26:16 +0000

    Chapter 5: display relative times.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
+import Post from './Post';
 
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
@@ -28,17 +29,7 @@ export default function Posts() {
                     {posts === null ?
                         <p>Could not retrieve blog posts.</p>
                     :
-                        
-                        posts.map(post => {
-                            return (
-                                <p key={post.id}>
-                                <b>{post.author.username}</b> &mdash; {post.timestamp}
-                                <br />
-                                {post.text}
-                                </p>
-                            );
-                        })
-                        
+                        posts.map(post => <Post key={post.id} post={post} />)                   
                     }
                 </>
             }
