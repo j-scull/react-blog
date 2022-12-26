687c107778ae5d1a7d793e691d2dbb1c90f2a44e
j-scull <joe-scullion@hotmail.com>
2022-12-26 18:45:58 +0000

    Chapter 6: add pagination.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 import { useApi } from "../contexts/ApiProvider";
+import More from "./More";
 import Post from './Post';
 
 const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
@@ -8,6 +9,7 @@ const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 export default function Posts({ content }) {
 
     const [posts, setPosts] = useState();
+    const [pagination, setPagination] = useState();
     const api = useApi();
 
     let url;
@@ -28,11 +30,22 @@ export default function Posts({ content }) {
         const response = await api.get(url);
         if (response.ok) {
           setPosts(response.body.data);
+          setPagination(response.body.pagination);
         } else {
           setPosts(null);
         }
       })();
     }, [api, url]);
+
+    const loadNextPage = async () => {
+        const response = await api.get(url, {
+          after: posts[posts.length - 1].timestamp
+        });
+        if (response.ok) {
+          setPosts([...posts, ...response.body.data]);
+          setPagination(response.body.pagination);
+        }
+    };
 
     return (
         <>
@@ -43,7 +56,14 @@ export default function Posts({ content }) {
                     {posts === null ?
                         <p>Could not retrieve blog posts.</p>
                     :
-                        posts.map(post => <Post key={post.id} post={post} />)                   
+                      <>
+                        {posts.length === 0 ?
+                          <p>There are no blog posts.</p>
+                        :
+                          posts.map(post => <Post key={post.id} post={post} />)                   
+                        }
+                        <More pagination={pagination} loadNextPage={loadNextPage} />
+                      </>
                     }
                 </>
             }
